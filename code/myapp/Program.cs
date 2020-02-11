e57285ad91e617a838e4294d00e4db28e1410d32
jonjelliott@gmail.com <45976921+jonjelliott1@users.noreply.github.com>
2020-02-11 12:54:02 -0500

    Added struct example

--- code/myapp/Program.cs
+++ code/myapp/Program.cs
@@ -7,6 +7,15 @@ namespace myapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // //Simple use of a structure
+            // int num;
+            // string snum;
+            // Console.Write("Enter a number: ");
+            // snum = Console.ReadLine();
+            // num = Int32.Parse(snum);
+            // Console.WriteLine(num);
+
         }
     }
 }
