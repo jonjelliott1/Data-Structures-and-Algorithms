84c2305ebbbcd16e52081b9210e4d7a58be6f137
jonjelliott@gmail.com <45976921+jonjelliott1@users.noreply.github.com>
2020-02-12 09:48:40 -0500

    Added Sorting Algorithms Project, and added Bubble Sort example

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -6,8 +6,8 @@ class class1
         var Jan = new int[31];
         var Feb = new int[29];
         int[][] sales = new int[2][];
-       sales[0] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
-       sales[1] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        sales[0] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        sales[1] = new int[] { 0, 0, 0, 0, 0, 0, 0 };
         int month, day, total;
         double average = 0.0;
         sales[0][0] = 41;
@@ -37,5 +37,23 @@ class class1
             Console.WriteLine("Average sales for month: " +
             month + ":" + average);
         }
+        //Example of Jagged Array
+        int[][,] jaggedArray4 = new int[3][,]
+                {
+                    new int[,] { {1,3}, {5,7} },
+                    new int[,] { {0,2}, {4,6}, {8,10} },
+                    new int[,] { {11,22}, {99,88}, {0,9} }
+                };
+
+        System.Console.Write("{0}", jaggedArray4[0][1, 0]);
+
+        System.Console.WriteLine(jaggedArray4.Length);
+
+        System.Console.Write("{0}", jaggedArray4[0][0, 1]); // 3
+        System.Console.Write("{0}", jaggedArray4[1][2, 1]); // 10
+        System.Console.Write("{0}", jaggedArray4[2][1, 1]); // 88
+
+
+
     }
 }
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Sorting algorithms you should study first...
+    //        // 1. Insertion sort
+    //        // 2. bublle sort
+    //        // 3. selection sort
+
+            
+            
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+    //array class test bed
+
+    class CArray
+    {
+        private int[] arr;
+        private int upper;
+        private int numElements;
+        public CArray(int size)
+        {
+            arr = new int[size];
+            upper = size - 1;
+            numElements = 0;
+
+        }
+        public void Insert(int item)
+        {
+            arr[numElements] = item;
+            numElements++;
+
+        }
+    
+    public void DisplayElements()
+        {
+            for (int i = 0; i <= upper; i++)
+                Console.Write(arr[i] + " ");
+        }
+    
+        public void Clear()
+        {
+            for (int i = 0; i <= upper; i++)
+                arr[i] = 0;
+            numElements = 0;
+
+        }
+
+        public void BubbleSort()
+        {
+            int temp;
+            for (int outer = upper; outer >= 1; outer--)
+            {
+                Console.WriteLine("Start Outer for"+ "inner: " + outer);
+                // if inner value is less than or equal to outer value
+                // or if first value is greater than last
+                for (int inner = 0; inner <= outer - 1; inner++)
+                    //if first is greater than second then
+                    if ((int)arr[inner] > arr[inner + 1])
+                    {
+                        //This swaps the item on the left with the item adjacent on the right
+                        //set first to temp
+                        Console.WriteLine("Swapping " + arr[inner] + " in loc: " +inner+ " with " + arr[inner + 1] + " in loc: " + (inner + 1));
+                        temp = arr[inner];
+                        //move adjecent to first to first
+                        arr[inner] = arr[inner + 1];
+                        //set item that was in first into the place second was in.
+                        arr[inner + 1] = temp;
+                        
+                        
+                    }
+                Console.WriteLine("End Outer for" + "outer: " + outer);
+                this.DisplayElements();
+            }
+        }
+        ////array of numbers 0 - 49
+        //static void Main()
+        //{
+        //    CArray nums = new CArray(50);
+        //    for (int i = 0; i <= 49; i++)
+        //        nums.Insert(i);
+        //    nums.DisplayElements();
+        //}
+
+        //array of 10 random numbers
+        static void Main()
+        {
+            CArray nums = new CArray(50);
+            Random rnd = new Random(100);
+            for (int i = 0; i < 50; i++)
+                nums.Insert((int)(rnd.NextDouble() * 100));
+            nums.DisplayElements();
+            nums.BubbleSort();
+            Console.WriteLine();
+            nums.DisplayElements();
+        }
+
+
+    }
+    
+}
